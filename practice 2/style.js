92dbe750feb97408c7bb47eff61c3465d105ffcd
Rana Sobaan <124592143+RanaSobaanshahid@users.noreply.github.com>
2023-05-16 18:33:14 +0500

    Add files via upload

---
+++ practice 2/style.js
@@ -0,0 +1,34 @@
+
+// 1--how we can access html elements using dom in javascript?
+
+// 1.getElementById: This method retrieves an element based on its unique id attribute.
+
+const elements=document.getElementById('id')
+
+// 2.getElementsByClassName: This method returns a collection of elements that have a specific class name.
+
+const elements=document.getElementsByClassName('classname')
+
+// 3.getElementsByTagName: This method returns a collection of elements with the specified tag name
+
+const elements = document.getElementsByTagName('tagName');
+
+// 4.querySelector: This method returns the first element that matches a specified CSS selector.
+
+const element = document.querySelector('selector');
+
+// 5.querySelectorAll: This method returns a collection of elements that match a specified CSS selector.
+
+const elements = document.querySelectorAll('selector');
+
+// 6.getElementsByName: This method returns a collection of elements with a specific name attribute.
+
+const elements = document.getElementsByName('elementName');
+
+//2-- What are the activities that we can using DOM?
+1.Accessing elements
+2.Modifying content
+3.Creating new elements
+4.Modifying styles
+5.Adding and removing classes
+6.Responding to events
